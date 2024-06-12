2ed822924a6d5037969b52536b0bd359a4372200
Nicolas Yax <nyax@Ordinateur-portable-de-Nicolas.local>
2024-06-12 11:49:21 +0200

    Added data download instructions

---
+++ download_data.sh
@@ -0,0 +1,17 @@
+#open-web-math
+wget https://huggingface.co/datasets/EleutherAI/proof-pile-2/blob/main/open-web-math/test/test.jsonl.zst -O data/open-web-math.jsonl.zst
+
+#MBXP
+wget https://github.com/amazon-science/mxeval/blob/main/data/mbxp/mbcpp_release_v1.2.jsonl -O data/mbxp/0000(0).jsonl
+wget https://github.com/amazon-science/mxeval/blob/main/data/mbxp/mbcsp_release_v1.2.jsonl -O data/mbxp/0000(1).jsonl
+wget https://github.com/amazon-science/mxeval/blob/main/data/mbxp/mbcgp_release_v1.2.jsonl -O data/mbxp/0000(2).jsonl
+wget https://github.com/amazon-science/mxeval/blob/main/data/mbxp/mbcplp_release_v1.2.jsonl -O data/mbxp/0000(3).jsonl
+wget https://github.com/amazon-science/mxeval/blob/main/data/mbxp/mbcscp_release_v1.2.jsonl -O data/mbxp/0000(4).jsonl
+wget https://github.com/amazon-science/mxeval/blob/main/data/mbxp/mbcswp_release_v1.2.jsonl -O data/mbxp/0000(5).jsonl
+wget https://github.com/amazon-science/mxeval/blob/main/data/mbxp/mbpp_release_v1.2.jsonl -O data/mbxp/0000(6).jsonl
+wget https://github.com/amazon-science/mxeval/blob/main/data/mbxp/mbjp_release_v1.2.jsonl -O data/mbxp/0000(7).jsonl
+wget https://github.com/amazon-science/mxeval/blob/main/data/mbxp/mbjsp_release_v1.2.jsonl -O data/mbxp/0000(8).jsonl
+wget https://github.com/amazon-science/mxeval/blob/main/data/mbxp/mbtsp_release_v1.2.jsonl -O data/mbxp/0000(9).jsonl
+wget https://github.com/amazon-science/mxeval/blob/main/data/mbxp/mbkp_release_v1.2.jsonl -O data/mbxp/0000(10).jsonl
+wget https://github.com/amazon-science/mxeval/blob/main/data/mbxp/mbrbp_release_v1.2.jsonl -O data/mbxp/0000(11).jsonl
+wget https://github.com/amazon-science/mxeval/blob/main/data/mbxp/mbphp_release_v1.2.jsonl -O data/mbxp/0000(12).jsonl
